703ef308ef03d84b5e381c94c985c241aa45894f
Helene <helene.mazgaldi@gmail.com>
2023-05-31 15:09:01 +0200

    Admin modif + suppr campus

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
 
-   // #[IsGranted("ROLE_ADMIN")]
+    #[IsGranted("ROLE_ADMIN")]
     #[Route('/campus', name: 'campus_list')]
     public function listCampus(Request $request, CampusRepository $campusRepository): Response
     {
@@ -26,8 +26,7 @@ class AdminController extends AbstractController
         $campuForm = $this->createForm(CampusType::class, $campu);
         $campuForm->handleRequest($request);
 
-
-        if ($campuForm->isSubmitted() && $campuForm->isValid()){
+        if ($campuForm->isSubmitted() && $campuForm->isValid()) {
             $campusRepository->add($campu, true);
 
             //après ajout, redirige vers la page de détail
@@ -38,39 +37,52 @@ class AdminController extends AbstractController
 
         return $this->render('admin/adcampus.html.twig', [
             'campus' => $campus,
-            'campuForm' => $campuForm->createView()
+            'campuForm' => $campuForm->createView(),
         ]);
     }
 
     #[Route('/campus/update/{id}', name: 'campus_update', requirements: ["id" => "\d+"])]
-    public function upCampus(CampusRepository $campusRepository): Response
+    public function upCampus(Request $request, int $id, CampusRepository $campusRepository): Response
     {
 
         //affichage des campus
-        $campus = $campusRepository->findAll();
+        $campus = $campusRepository->find($id);
 
 
+        $campuForm = $this->createForm(CampusType::class, $campus);
+        $campuForm->handleRequest($request);
 
+        if ($campuForm->isSubmitted() && $campuForm->isValid()) {
 
+            $campusRepository->add($campus, true);
 
+            //après ajout, redirige vers la page de détail
 
+            $this->addFlash('success', 'Campus modifié !');
 
+            return $this->redirectToRoute('admin_campus_list');
+        }
         return $this->render('admin/adcampus.html.twig', [
             'campus' => $campus,
+            'campuForm' => $campuForm->createView(),
         ]);
+
     }
 
     #[Route('/campus/delete/{id}', name: 'campus_delete', requirements: ["id" => "\d+"])]
-    public function delCampus(CampusRepository $campusRepository): Response
+    public function delCampus(int $id, CampusRepository $campusRepository): Response
     {
-        $campus = $campusRepository->findAll();
+        //on récupère le campus à supprimer
+        $campus = $campusRepository->find($id);
+
+        //on supprime la série
+        $campusRepository->remove($campus, true);
+
+        $this->addFlash('success', $campus->getName() . ' a été supprimé !');
 
 
+        return $this->redirectToRoute('admin_campus_list');
 
-
-        return $this->render('admin/adcampus.html.twig', [
-            'campus' => $campus,
-        ]);
     }
 
 
