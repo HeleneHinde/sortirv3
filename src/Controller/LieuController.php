552bd5adef2c0bd8b4f8d9e463059571a5e70069
gjohanet2023 <gaspard.johanet@gmail.com>
2023-06-02 10:22:13 +0200

    gestion lieux

---
+++ src/Controller/LieuController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use App\Form\LieuType;
+use App\Form\SortiesType;
+use App\Repository\LieuRepository;
+use App\Repository\VilleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/lieu', name: 'lieu_')]
+class LieuController extends AbstractController
+{
+    #[Route('/', name: 'list')]
+    public function list(LieuRepository $lieuRepository): Response
+    {
+        $lieux = $lieuRepository->findAll();
+
+
+        return $this->render('lieu/list.html.twig', [
+            'lieux' => $lieux,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', requirements: ["id" => "\d+"])]
+    public function show(LieuRepository $lieuRepository, int $id): Response
+    {
+        $lieu = $lieuRepository->find($id);
+
+
+        return $this->render('lieu/show.html.twig', [
+            'lieu' => $lieu,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'delete', requirements: ["id" => "\d+"])]
+    public function delete(LieuRepository $lieuRepository, int $id): Response
+    {
+        $lieu = $lieuRepository->find($id);
+
+        $lieuRepository->remove($lieu, true);
+
+        $this->addFlash('success', $lieu->getNom() . ' a été supprimé !');
+
+        return $this->redirectToRoute('lieu_list');
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(LieuRepository $lieuRepository, VilleRepository $villeRepository, Request $request): Response
+    {
+        $lieu = new Lieu();
+        $lieuForm = $this->createForm(LieuType::class, $lieu);
+
+        $lieuForm->handleRequest($request);
+
+
+        if ($lieuForm->isSubmitted() && $lieuForm->isValid()){
+
+            if ($request->request->get('myCheckboxVille')){
+                $ville = new Ville();
+                $ville->setName($request->request->get('lieu')['Ville']);
+                $ville->setCp($request->request->get('lieu')['Code_postal']);
+
+                $villeRepository->add($ville, true);
+
+                $lieu->setVille($ville);
+
+            } else {
+                $lieu->setVille($villeRepository->find($lieuForm->get('ville')->getData()));
+            }
+
+            $lieuRepository->add($lieu, true);
+
+            $this->addFlash('succès', 'Lieu "'. $lieu->getNom() .'" ajouté avec succès');
+
+
+            return $this->redirectToRoute('lieu_list');
+        }
+
+        return $this->render('lieu/create.html.twig', [
+            'lieuForm' => $lieuForm->createView(),
+        ]);
+    }
+
+    #[Route('/update/{id}', name: 'update', requirements: ["id" => "\d+"])]
+    public function edit(LieuRepository $lieuRepository, VilleRepository $villeRepository,Request $request, int $id): Response
+    {
+        $lieu = $lieuRepository->find($id);
+
+        $lieuForm2 = $this->createForm(LieuType::class, $lieu);
+
+        $lieuForm2->handleRequest($request);
+
+
+        if ($lieuForm2->isSubmitted() && $lieuForm2->isValid()){
+
+            if ($request->request->get('myCheckboxVille')){
+                $ville = new Ville();
+                $ville->setName($request->request->get('lieu')['Ville']);
+                $ville->setCp($request->request->get('lieu')['Code_postal']);
+
+                $villeRepository->add($ville, true);
+
+                $lieu->setVille($ville);
+
+            } else {
+                $lieu->setVille($villeRepository->find($lieuForm2->get('ville')->getData()));
+            }
+
+            $lieuRepository->add($lieu, true);
+
+            $this->addFlash('succès', 'Lieu "'. $lieu->getNom() .'" modifié avec succès');
+
+
+            return $this->redirectToRoute('lieu_list');
+        }
+
+        return $this->render('lieu/edit.html.twig', [
+            'lieuForm' => $lieuForm2->createView(),
+            'lieu' => $lieu
+        ]);
+    }
+}
---
+++ src/Form/LieuType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('rue')
+            ->add('ville', EntityType::class, [
+                'label' => 'Ville',
+                'class' => Ville::class,
+                'choice_label' => 'name',
+                'choice_value' => 'id'
+            ])
+            ->add('Ville', TextType::class, [
+                'mapped' => false,
+                'disabled' => true
+            ])
+            ->add('Code_postal', NumberType::class, [
+                'mapped' => false,
+                'label' => 'Code postal',
+                'disabled' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
